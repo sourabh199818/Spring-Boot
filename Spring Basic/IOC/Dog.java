e67d2a487bd504bd670ec945ce5fc4b497a6b768
sourabh199818 <68646654+sourabh199818@users.noreply.github.com>
2022-06-28 11:53:58 +0530

    Create Dog.java

---
+++ Spring Basic/IOC/Dog.java
@@ -0,0 +1,20 @@
+package Hello; 
+
+public  class Dog implements Source
+
+{
+	@Override
+public  void bark()
+ {
+	 System.out.println("I'm inside  dog bark method");
+	 
+ }
+	@Override
+	public void smell()
+	{
+		System.out.println("I'm inside  Dog smell Data");
+	}
+	
+	
+	
+}
