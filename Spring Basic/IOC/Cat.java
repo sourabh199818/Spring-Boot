1a7a56b15f37d929c57b80d193322eabdcda8f8b
sourabh199818 <68646654+sourabh199818@users.noreply.github.com>
2022-06-28 11:55:03 +0530

    Create Cat.java

---
+++ Spring Basic/IOC/Cat.java
@@ -0,0 +1,20 @@
+package Hello;
+
+public class Cat implements Source {
+	
+	 {
+		 System.out.println("I'm inside  Cat Meow method");
+		 
+	 }
+		@Override
+		public void smell()
+		{
+			System.out.println("I'm inside  cat smell  method ");
+		}
+		@Override
+		public void bark() {
+			// TODO Auto-generated method stub
+			
+		}
+
+}
